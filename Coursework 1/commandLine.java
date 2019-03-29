654804a46b108d2a414477c7296346f2fae52149
PetercSnk <pcsnook@btinternet.com>
2019-03-29 13:55:16 +0000

    cmd line

--- Coursework 1/commandLine.java
+++ Coursework 1/commandLine.java
@@ -3,25 +3,36 @@ import java.util.Scanner;
 public class commandLine {
 	public static void main( String[] args ) {
 
+		boolean running = true;
+
+		while ( running ) {
+
 		Scanner in = new Scanner(System.in);
-		System.out.println("Do you want to SAVE to a file or LOAD a file: ");
-		String choice = in.nextString(); 
+		System.out.print( "Do you want to SAVE to a file or LOAD a file: " );
+		String choice = in.nextLine(); 
 
-		if (choice == "save") {
+		if ( choice.equals( "save" )) {
+			System.out.println( "save" );
+
+
+
+
+		} else if ( choice.equals( "load" )) {
+			System.out.println( "load" );
+
+		} else if ( choice.equals( "exit" )) {
+			System.out.println( "exit" );
+			running = false;
+
+
+		} else {
+			System.out.println( "invalid" );
+		}
+
+
+
 
 
 		}
-
-		else if (choice == "load") {
-
-
-
-		}
-
-		else if {
-			return "invalid"
-		}
-
-
-	}
+	}	
 }
